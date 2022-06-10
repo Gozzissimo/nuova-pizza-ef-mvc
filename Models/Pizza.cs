9a9904cf420ff4030c4956280866362f4a5484bc
Gozzissimo <fantini.nico@gmail.com>
2022-06-10 14:06:30 +0200

    Aggiungere i file di progetto.

---
+++ Models/Pizza.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace nuova_pizza_ef_mvc.Models
+{
+    [Table("Pizza")]
+    public class Pizza
+    {
+        public int? Id { get; set; }
+        [Required(ErrorMessage = "Nome richiesto")]
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? ImgPath { get; set; }
+        [Required(ErrorMessage = "Prezzo richiesto")]
+        public string? Price { get; set; }
+        [NotMapped]
+        public IFormFile? formFile { get; set; }
+        public Pizza() { }
+    }
+}
